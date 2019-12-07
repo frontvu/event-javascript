9ca8a2d4ff4f8f87ff36d90d81211baea905dc66
Jason Matthews <jason@netprotocal.com>
2019-12-06 22:26:31 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,41 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+$(function () {
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+    function hideError() {
+        $("#loginerror").hide()
+    }
+
+    function login() {
+        var password = "pass"
+        var enteredpassword = $("#passwordinput").prop("value")
+
+        if (password === enteredpassword) {
+            $("#loginerror").hide()
+            $("#main").show()
+            $("#login").hide()
+        } else {
+            $("#loginerror").show()
+        }
+    }
+
+    function fadeOut() {
+        var image = $(this)
+
+        image.fadeTo("fast", 0.5)
+        image.css("border-color","orange")
+    }
+
+    function fadeIn() {
+        var image = $(this)
+
+        image.fadeTo("fast", 1)
+        image.css("border-color","white")
+    }
+
+    $("#passwordinput").focus(hideError)
+
+    $("#loginbutton").click(login)
+
+    $(".images img").mouseover(fadeOut)
+
+    $(".images img").mouseout(fadeIn)
+})
